e220939ba9d2915e076af7c9da06f3cbfd5fe503
Christopher Jenks <christopherjenks@Christophers-MacBook-Pro.local>
2017-10-12 14:44:51 +0100

    Add slider to homepage

---
+++ wp-content/themes/understrap/page-templates/homepage.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Home Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package understrap
+ */
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="slider">
+	<?php echo do_shortcode("[metaslider id=19]"); ?>
+</div>
+
+<div class="wrapper" id="full-width-page-wrapper">
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+		<div class="row">
+			<div class="col">
+				<?php echo do_shortcode( '[adverts_list]' ); ?>
+			</div>
+		</div><!-- .row end -->
+	</div><!-- Container end -->
+</div><!-- Wrapper end -->
+
+<?php get_footer(); ?>
