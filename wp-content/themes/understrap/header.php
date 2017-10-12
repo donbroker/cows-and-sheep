289c892f654377f5aafbdf61b1e49f4e197c13f8
Christopher Jenks <christopherjenks@Christophers-MacBook-Pro.local>
2017-10-12 14:33:55 +0100

    Modify header to include social and login links

--- wp-content/themes/understrap/header.php
+++ wp-content/themes/understrap/header.php
@@ -20,6 +20,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
@@ -27,12 +28,30 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="header">
 	<div class="container">
 		<div class="row">
-			<div class="col">
-				<img src="<?php echo site_url(); ?>/images/cows-and-sheep-logo.jpg" alt="" />
+			<div class="col logo">
+				<img src="<?php echo site_url(); ?>/images/cows-and-sheep-logo.png" alt="" />
 			</div>
-			<div class="col">
+			<div class="col login">
+				<div class="content">
+					<a href="#">+ login</a> | <a href="#">+ sign up</a>
+				</div>
 			</div>
-			<div class="col">
+			<div class="col social">
+				<div class="content">
+					<div class="nav-menu">
+						<div class="menu">
+							<a href="#">+ Help</a> | <a href="#">+ My Account</a>
+						</div>
+					</div>
+					<div class="nav-social">
+						<ul>
+							<li><a href="#"><img src="<?php echo site_url(); ?>/images/cows-and-sheep-facebook.png" /></a></li>
+							<li><a href="#"><img src="<?php echo site_url(); ?>/images/cows-and-sheep-twitter.png" /></a></li>
+							<li><a href="#"><img src="<?php echo site_url(); ?>/images/cows-and-sheep-linkedin.png" /></a></li>
+							<li><a href="#"><img src="<?php echo site_url(); ?>/images/cows-and-sheep-google-plus.png" /></a></li>
+						</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
